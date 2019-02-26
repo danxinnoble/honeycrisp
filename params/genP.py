ff57e0eb21dd810dcebdc325cb061d5e81e08b8d
danxinnoble <danielgeorgenoble@gmail.com>
2019-02-25 22:03:18 -0500

    Make genP.py use same naming conventions as other programs. Update prime (and root of unity) to support lgM=50.

--- params/genP.py
+++ params/genP.py
@@ -1,4 +1,4 @@
-# A simple script for finding a large prime, p, such that p-1 is divisible by a high power of 2
+# A simple script for finding a lgP-bit prime, p, such that p-1 is divisible by 2**(lgM + 1)
 
 import sympy
 import random 
@@ -10,6 +10,6 @@ p = 4
 lgM = 50
 
 while (not sympy.isprime(p)):
-  p = random.randrange(2**(lgP-lgM-1), 2**(lgP-lgM)-1) * 2**lgM +1
+  p = random.randrange(2**(lgP-lgM-2), 2**(lgP-lgM-1)-1) * 2**(lgM+1) +1
 
 print p
