92d28455eb0580fbb9b23c0e87eb1a96c90a338a
danxinnoble <danielgeorgenoble@gmail.com>
2019-03-15 17:24:56 -0400

    Allow passing command-line args to benchmark script, also made output cleaner.

--- source/benchmark.sh
+++ source/benchmark.sh
@@ -1,10 +1,12 @@
-PROG_NAME=Programs/lwe_test
+#!/bin/bash
+
+PROG_NAME=$1
 N_PLAYERS=4
-N_TRIPLES=512
-N_BITS=0
+N_TRIPLES=$2
+N_BITS=$3
 N_SQUARES=0
 
-cat /proc/net/dev | grep lo
+echo 'Measuring the runtime and communication cost of' $PROG_NAME
 
 COMM_T0=$(cat /proc/net/dev | grep -o lo..\[0-9\]* | grep -o \[0-9\]*)
 
@@ -12,10 +14,7 @@ for i in {0..2}; do
   ./Player.x -max ${N_TRIPLES},${N_BITS},${N_SQUARES} $i $PROG_NAME > /dev/null 2> /dev/null &
 done
 
-time (./Player.x -max ${N_TRIPLES},${N_BITS},${N_SQUARES} 3 $PROG_NAME > /dev/null 2> /dev/null )
+time (./Player.x -max ${N_TRIPLES},${N_BITS},${N_SQUARES} 3 $PROG_NAME > /dev/null 2> /dev/null ) 
 
-cat /proc/net/dev | grep lo
 COMM_T1=$(cat /proc/net/dev | grep -o lo..\[0-9]\* | grep -o \[0-9\]*)
-
-
 echo 'Communication Cost (bytes):' $(($COMM_T1 - $COMM_T0))
